28342fc8f1de9716d10cac31b852e99d6dde32a2
Musab36 <musabhd@gmail.com>
2017-11-23 23:51:42 +0300

    QR code reader

--- app/src/main/java/com/salajim/musab/demo/ui/MainActivity.java
+++ app/src/main/java/com/salajim/musab/demo/ui/MainActivity.java
@@ -8,11 +8,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.salajim.musab.demo.R;
 import com.salajim.musab.demo.adapters.LogosAdapter;
 import com.salajim.musab.demo.models.Demo;
 import com.salajim.musab.demo.services.DemoService;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -28,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     private LogosAdapter mAdapter;
 
+    //qr code scanner object
+    private IntentIntegrator qrScan;
+
     public ArrayList<Demo> mDemos = new ArrayList<>();
 
     @Override
@@ -36,10 +45,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
         Intent intent = getIntent();
         String demos = intent.getStringExtra("demos");
         getLogos(demos);
 
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    //textViewName.setText(obj.getString("name"));
+                    //textViewAddress.setText(obj.getString("address"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     //Inflating Menus
@@ -55,6 +97,11 @@ public class MainActivity extends AppCompatActivity {
     //Menu click listener
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.qr_code) {
+            //initiating the qr code scan
+            qrScan.initiateScan();
+        }
 
         return super.onOptionsItemSelected(item);
     }
